38652f68ee3602a7f6fecc3d486b19bd4cc6e9a6
nuskirk <kirk@nustechnology.com>
2022-04-29 03:11:20 +0700

    Testing Messages APIs

--- api/routes/v1.ts
+++ api/routes/v1.ts
@@ -5,7 +5,7 @@ const versionPath = '/v1';
 
 const messageTransporter = new MessageTransporter();
 const messageRouter = routerV1.route(`${versionPath}/messages`);
-messageRouter.all(messageTransporter.list);
+messageRouter.get(messageTransporter.list);
 messageRouter.post(messageTransporter.create);
 
 export default routerV1;
